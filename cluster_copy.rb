5a3e723ed9eecd0b94a15499eaa9eec3e5066cf8
Nesurion <maik.schwan@gmail.com>
2015-04-03 00:03:05 +0200

    copy uebung or aufgabe or file

--- cluster_copy.rb
+++ cluster_copy.rb
@@ -7,13 +7,30 @@ cluster_username = ENV['CLUSTER_USERNAME']
 
 full_path = `pwd`[0..-2]
 path_array = full_path.split('/')
-file_name = ARGV[0]
+arg = ARGV[0]
 
-uebung = path_array[-2]
-aufgabe = path_array[-1]
+level1 = path_array[-3]
+level2 = path_array[-2]
+level3 = path_array[-1]
 
-if ( uebung =~ /u[0-9]/ and aufgabe =~ /a[0-9]/ )
-  system("scp #{file_name} #{cluster_username}@wr0.wr.inf.h-brs.de:parprog/#{uebung}/#{aufgabe}")
+p "level1: #{level1}"
+p "level2: #{level2}"
+p "level3: #{level3}"
+p "arg: #{arg}\n"
+
+if level3 == "parprog"
+  # farly safe to assume we are in the right folder and user intended to copy a whole uebung folder
+  # create uebung folder
+  system("rsync -ave ssh #{arg} #{cluster_username}@wr0.wr.inf.h-brs.de:parprog/")
+end
+
+if level3 =~ /u[0-9]/
+  # source is a uebung folder and we want to copy a whole aufgabe folder
+  system("rsync -ave ssh #{arg} #{cluster_username}@wr0.wr.inf.h-brs.de:parprog/#{level3}")
+elsif level2 =~ /u[0-9]/ and level3 =~ /a[0-9]/ 
+  # source is a file in a level3 folder and we copy to realted folder on cluster
+  # cant copy single file if uebung folder on cluster doesn't exist
+  system("rsync -ave ssh #{arg} #{cluster_username}@wr0.wr.inf.h-brs.de:parprog/#{level2}/#{level3}/")
 else
-  p "path doesn't path. be sure to use this in .../uX/aY"
+  p "path doesn't match. be sure to copy eighter a aufgabe file, aufgabe folder or u2 folder."
 end
