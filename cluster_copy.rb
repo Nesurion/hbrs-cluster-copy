45f6524012993c5d8cc864d2bd3f091e22eb215b
Nesurion <maik.schwan@gmail.com>
2015-04-02 23:12:03 +0200

    make sure workdir is right

--- cluster_copy.rb
+++ cluster_copy.rb
@@ -2,7 +2,6 @@
 # append your ssh public key "~/.ssh/id_rsa.pub" to "~/.ssh/authorized_keys"
 # local folder structure needs to be the same as on cluster
 # parprog/uX/aY
-require "json"
 
 cluster_username = ENV['CLUSTER_USERNAME']
 
@@ -13,4 +12,8 @@ file_name = ARGV[0]
 uebung = path_array[-2]
 aufgabe = path_array[-1]
 
-system("scp #{file_name} #{cluster_username}@wr0.wr.inf.h-brs.de:parprog/#{uebung}/#{aufgabe}")
+if ( uebung =~ /u[0-9]/ and aufgabe =~ /a[0-9]/ )
+  system("scp #{file_name} #{cluster_username}@wr0.wr.inf.h-brs.de:parprog/#{uebung}/#{aufgabe}")
+else
+  p "path doesn't path. be sure to use this in .../uX/aY"
+end
